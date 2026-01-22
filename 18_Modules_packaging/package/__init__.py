20e02387c960ff2d19a40c46ced5a1bf17d8588f
DeepakYadav9892 <deepak989241@gmail.com>
2026-01-22 20:01:57 +0530

    secondcommitof22jan

---
+++ 18_Modules_packaging/package/__init__.py
@@ -0,0 +1,5 @@
+"""
+Docstring for 18_Modules_packaging.package
+__init__.py is a special file in python to define
+packages and initialize their namespaces .
+"""
---
+++ 18_Modules_packaging/test.py
@@ -0,0 +1,5 @@
+from package.maths import*
+print(addition(4,5))
+print(substraction(100,50))
+from package.subpackages.mult import multiply
+print(multiply(8,9))
